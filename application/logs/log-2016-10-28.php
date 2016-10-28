45366c0b7914f62a1ca423d4f6eac43da5290db3
Staffan <enemark51@hotmail.com>
2016-10-28 13:37:28 +0200

    Changed back to files before thirdparty plugin

--- application/logs/log-2016-10-28.php
+++ application/logs/log-2016-10-28.php
@@ -30,3 +30,44 @@ ERROR - 2016-10-28 13:26:45 --> 404 Page Not Found: Hello%20World/index
 DEBUG - 2016-10-28 13:26:58 --> UTF-8 Support Enabled
 DEBUG - 2016-10-28 13:26:58 --> Global POST, GET and COOKIE data sanitized
 ERROR - 2016-10-28 13:26:58 --> 404 Page Not Found: Login/index
+DEBUG - 2016-10-28 13:34:42 --> UTF-8 Support Enabled
+ERROR - 2016-10-28 13:34:42 --> Severity: Notice --> Use of undefined constant LOGIN_PAGE - assumed 'LOGIN_PAGE' C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\config\routes.php 56
+DEBUG - 2016-10-28 13:34:42 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:34:42 --> 404 Page Not Found: /index
+DEBUG - 2016-10-28 13:35:20 --> UTF-8 Support Enabled
+ERROR - 2016-10-28 13:35:20 --> Severity: Notice --> Use of undefined constant LOGIN_PAGE - assumed 'LOGIN_PAGE' C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\config\routes.php 56
+DEBUG - 2016-10-28 13:35:20 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:35:20 --> 404 Page Not Found: /index
+DEBUG - 2016-10-28 13:36:21 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:21 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:36:21 --> 404 Page Not Found: /index
+DEBUG - 2016-10-28 13:36:41 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:41 --> No URI present. Default controller set.
+DEBUG - 2016-10-28 13:36:41 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:36:41 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:36:41 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-10-28 13:36:41 --> Total execution time: 0.6760
+DEBUG - 2016-10-28 13:36:42 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:42 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:36:42 --> 404 Page Not Found: Hello%20World/index
+DEBUG - 2016-10-28 13:36:47 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:36:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:36:47 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-10-28 13:36:47 --> Total execution time: 0.1256
+DEBUG - 2016-10-28 13:36:47 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:36:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:36:47 --> Total execution time: 0.0500
+DEBUG - 2016-10-28 13:36:47 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:47 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:36:47 --> 404 Page Not Found: Hello%20World/index
+DEBUG - 2016-10-28 13:36:47 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:47 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:36:47 --> 404 Page Not Found: Hello%20World/index
+DEBUG - 2016-10-28 13:36:50 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:36:50 --> No URI present. Default controller set.
+DEBUG - 2016-10-28 13:36:50 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:36:50 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:36:50 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-10-28 13:36:50 --> Total execution time: 0.5150
