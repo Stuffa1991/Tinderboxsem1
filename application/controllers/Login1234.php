128cfec32055270639fc25ba4a060a3975aab31a
Staffan <enemark51@hotmail.com>
2016-10-28 13:30:50 +0200

    Changes

--- application/controllers/Login1234.php
+++ application/controllers/Login1234.php
@@ -5,7 +5,7 @@ class Login1234 extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->load->library('form_validation', 'auth');
+		$this->load->library('auth');
 		$this->load->library('form_validation');
 		$this->load->model('login_model');
 		$this->load->helper('url');
---
+++ application/logs/log-2016-10-28.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-10-28 13:26:04 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:26:04 --> No URI present. Default controller set.
+DEBUG - 2016-10-28 13:26:04 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:26:04 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/db_tables.php
+DEBUG - 2016-10-28 13:26:04 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/authentication.php
+DEBUG - 2016-10-28 13:26:04 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:26:04 --> Encryption: Auto-configured driver 'openssl'.
+DEBUG - 2016-10-28 13:26:04 --> Total execution time: 0.3120
+DEBUG - 2016-10-28 13:26:04 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:26:04 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:26:04 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/db_tables.php
+DEBUG - 2016-10-28 13:26:04 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/authentication.php
+DEBUG - 2016-10-28 13:26:05 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:26:05 --> Encryption: Auto-configured driver 'openssl'.
+ERROR - 2016-10-28 13:26:05 --> Query error: Table 'tinderbox.ips_on_hold' doesn't exist - Invalid query: DELETE FROM `ips_on_hold`
+WHERE `time` < '2016-10-28 13:16:05'
+DEBUG - 2016-10-28 13:26:44 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:26:44 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-10-28 13:26:44 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/db_tables.php
+DEBUG - 2016-10-28 13:26:45 --> Config file loaded: C:\Users\Bruger\Dropbox\xampp\htdocs\tinderbox\application\third_party/community_auth/config/authentication.php
+DEBUG - 2016-10-28 13:26:45 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2016-10-28 13:26:45 --> Encryption: Auto-configured driver 'openssl'.
+DEBUG - 2016-10-28 13:26:45 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-10-28 13:26:45 --> Total execution time: 0.5130
+DEBUG - 2016-10-28 13:26:45 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:26:45 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:26:45 --> 404 Page Not Found: Hello%20World/index
+DEBUG - 2016-10-28 13:26:58 --> UTF-8 Support Enabled
+DEBUG - 2016-10-28 13:26:58 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2016-10-28 13:26:58 --> 404 Page Not Found: Login/index
