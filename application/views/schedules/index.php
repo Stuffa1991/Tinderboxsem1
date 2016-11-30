bc15df54c5718a3d9c4d54702493a893445a5605
chicco <Chic@Chics-MacBook-Pro.local>
2016-11-30 14:30:38 +0100

    schedules
    calender

--- application/views/schedules/index.php
+++ application/views/schedules/index.php
@@ -1,3 +1,5 @@
+
+
 <div class="container">
   <div class="row">
     <div class="col s12">
@@ -8,8 +10,77 @@
             <li class="tab col s3"><a href="#test2">Next 30 days</a></li>
           </ul>
         </div>
-        <div id="test1" class="col s12">Test 1</div>
-        <div id="test2" class="col s12">Test 2</div>
+        <div id="test1" class="col s12">
+	        
+		        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        	        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        	        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        	        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        	        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+		        	        <div class="row card-panel schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 08.09</div>
+		        </div>
+        </div>
+        
+        <div class="col s12" id="test2">
+        		
+        		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		           <div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+		                		<div class="row card-month schedule-farver">
+		        	<div class="col s6"> Mandag</div>
+		        	<div class="col s6 right-align"> 28th November 2016</div>
+		        </div>
+
+
+
+        </div>
+
       </div>
     </div>
   </div>
